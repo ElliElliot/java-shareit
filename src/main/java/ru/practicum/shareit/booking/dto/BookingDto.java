83ae562af2f1ca290f5f513fdd7a6bd63e01ebf4
Lesya S <lesaslo12@yandex.ru>
2023-03-12 15:55:09 +0300

    исправлены замечания

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -5,6 +5,7 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
+
 public class BookingDto {
     int id; // — уникальный идентификатор бронирования;
     LocalDateTime start; // — дата и время начала бронирования;
